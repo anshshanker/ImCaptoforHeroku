528a30f540ef6ecd8afbf02564b20da21490822c
anshshanker <shankeransh1999@gmail.com>
2021-08-08 02:55:48 +0530

    Update stream.py

--- stream.py
+++ stream.py
@@ -90,6 +90,7 @@ def load_image(image_file):
 	img = Image.open(image_file)
 	return img 
 st.set_option('deprecation.showfileUploaderEncoding',False)
+st.set_option('server.enableWebSocketCompression',False)
 st.title("Image Captioning")
 html_temp="""
 <div style="background-color:tomato;padding:10px">
